63d87c312c7d6cfd989e89293c4d17fa8bb7defa
hsiehwann <bni1998716@gmail.com>
2022-02-10 19:23:09 +0800

    carousel

--- app.py
+++ app.py
@@ -190,11 +190,36 @@ def getNameEmojiMessage():
 
 
 def getCarouselMessage(data):
-    message = dict()
+    message = {
+      "type": "template",
+      "altText": "this is a image carousel template",
+      "template": {
+          "type": "image_carousel",
+          "columns": [
+              {
+                "imageUrl": F"{end_point}/static/taipei_101.jpeg",
+                "action": {
+                  "type": "postback",
+                  "label": "台北101",
+                  "data": "action=buy&itemid=111"
+                }
+              },
+              {
+                "imageUrl": F"{end_point}/static/taipei_1.jpeg",
+                "action": {
+                  "type": "postback",
+                  "label": "台北101",
+                  "data": "action=buy&itemid=111"
+                }
+              }
+          ]
+      }
+    }
     return message
 
 
 def getLocationConfirmMessage(title, latitude, longitude):
+    data = {"title":title, "latitude":latitude, "logitude":longitude, "action":"get_near"}
     message = {
         "type": "template",
         "altText": "this is a confirm template",
@@ -203,14 +228,14 @@ def getLocationConfirmMessage(title, latitude, longitude):
             "text": f"確認是否搜尋 {title} 附近地點?",
             "actions": [
                 {
-                    "type": "message",
-                    "label": "Yes",
-                    "text": "yes"
+                  "type": "postback",
+                  "label": "是",
+                  "data": json.dumps(data),
                 },
                 {
                     "type": "message",
-                    "label": "No",
-                    "text": "no"
+                    "label": "否",
+                    "text": "否"
                 }
             ]
         }
